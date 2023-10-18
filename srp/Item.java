64149f53c1f4a64c2a7ee2a42a317673340203a4
Yoga <yoga21@if.unjani.ac.id>
2023-10-18 10:48:47 +0700

    menambah contoh srp

---
+++ srp/Item.java
@@ -0,0 +1,5 @@
+package srp;
+
+public class Item {
+    
+}
---
+++ srp/Order.java
@@ -0,0 +1,26 @@
+package srp;
+class Order{
+    void calculateTotalSum() {
+        /*... */
+    }
+    void getItems() { 
+        /*...*/ 
+    } 
+    void getItemCount() { 
+        /*...*/ 
+    } 
+    void addItem(Item item) { 
+        /*...*/ 
+    } 
+    void deleteItem(Item item) { 
+        /*...*/ 
+    } 
+    void printOrder() { 
+        /*...*/ 
+    } 
+    void showOrder() { 
+        /*...*/ 
+    } 
+    void getDailyHistory() {} 
+    void getMonthlyHistory() {}
+}
