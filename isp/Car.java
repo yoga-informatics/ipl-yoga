e164829b38b20df345925633e0ef04471689f57c
Yoga <yoga21@if.unjani.ac.id>
2023-11-04 15:27:02 +0700

    sebelum melakukan pattern mvc

--- isp/Car.java
+++ isp/Car.java
@@ -3,22 +3,14 @@ package isp;
 public class Car implements VehicleInterface,DoorInterface {
     @Override
     public void drive(){
-
     }
-
     @Override
     public void stop(){
-
     }
-
     @Override
     public void refuel(){
-
     }
-
     @Override
     public void openDoors(){
-
     }
-    
 }
