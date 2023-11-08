14b0454f70adc2f8ce5f27fac9755782a296e9d2
Yoga <yoga21@if.unjani.ac.id>
2023-11-08 10:54:21 +0700

    Commit informativeComments dan Clarification

---
+++ CleanCode/GoodComments/Clarification.java
@@ -0,0 +1,4 @@
+package GoodComments;
+
+assertTrue(a.compareTo(b) == -1); // a < b 
+assertTrue(b.compareTo(a) == 1); // b > a
---
+++ CleanCode/GoodComments/InformativeComments.java
@@ -0,0 +1,7 @@
+package GoodComments;
+
+// Returns an instance of the Responder being tested. 
+protected abstract Responder responderInstance(); 
+// renaming the function: responderBeingTested 
+// format matched kk:mm:ss EEE, MMM dd, yyyy 
+Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w* \\d*, \\d*");
