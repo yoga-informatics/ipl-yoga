efeb291e667e7938c7790048f77416861f5abaf6
Yoga <yoga21@if.unjani.ac.id>
2023-11-08 10:27:50 +0700

    Minesweeeper setelah perubahan

--- CleanCode/Minesweeper.java
+++ CleanCode/Minesweeper.java
@@ -1,7 +1,11 @@
-public List<int[]> getThem() { 
- List<int[]> list1 = new ArrayList<int[]>(); 
- for (int[] x : theList) 
- if (x[0] == 4) 
- list1.add(x); 
- return list1; 
-} 
+public class Minesweeper {
+    public final static int STATUS_VALUE = 0; 
+    public final static int FLAGGED = 4; 
+    public List<int[]> getFlaggedCells() { 
+    List<int[]> flaggedCells = new ArrayList<int[]>(); 
+    for (int[] cell : gameBoard) 
+    if (cell[STATUS_VALUE] == FLAGGED) 
+    flaggedCells.add(cell); 
+    return flaggedCells; 
+    }
+}
