1c0673ae16463955630a87236970889bb51fcb0a
Yoga <yoga21@if.unjani.ac.id>
2023-11-08 10:33:22 +0700

    Employee.java sebelum perubahan

---
+++ CleanCode/Eployee.java
@@ -0,0 +1,14 @@
+public class Eployee {
+    int payAmount() { 
+        switch (getType()) { 
+            case EmployeeType.ENGINEER: 
+                return _monthlySalary; 
+            case EmployeeType.SALESMAN: 
+                return _monthlySalary + _commission; 
+            case EmployeeType.MANAGER: 
+            return _monthlySalary + _bonus; 
+            default: 
+                throw new Exception("Incorrect Employee"); 
+            } 
+       } 
+}
