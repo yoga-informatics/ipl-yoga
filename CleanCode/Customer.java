65319857d086d6f324cbfa9069b953565770791b
Yoga <yoga21@if.unjani.ac.id>
2023-11-08 10:31:43 +0700

    customer.java setelah perubahan

--- CleanCode/Customer.java
+++ CleanCode/Customer.java
@@ -1,5 +1,6 @@
-class DtaRcrd102 { 
-    private Date genymdhms; 
-    private Date modymdhms; 
-    private final String pszqint = "102"; 
+class Customer { 
+    private Date generationTimestamp; 
+    private Date modificationTimestamp; 
+    private final String recordId = "102"; 
+
 }
